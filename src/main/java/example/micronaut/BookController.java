d6c98bf41adc181cd950bc265117d55257b8f65d
MiriamT <MiriamT@users.noreply.github.com>
2022-02-25 11:38:32 -0500

    init

---
+++ src/main/java/example/micronaut/BookController.java
@@ -0,0 +1,27 @@
+package example.micronaut;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@Controller
+public class BookController {
+
+    @Post
+    public BookSaved save(@Valid @Body Book book) {
+        BookSaved bookSaved = new BookSaved();
+        bookSaved.setName(book.getName());
+        bookSaved.setIsbn(UUID.randomUUID().toString());
+        return bookSaved;
+    }
+
+    @Post("custom")
+    public BookSaved saveCustom(@Valid @Body Book book) {
+        BookSaved bookSaved = new BookSaved();
+        bookSaved.setName("custom");
+        bookSaved.setIsbn(UUID.randomUUID().toString());
+        return bookSaved;
+    }
+}
